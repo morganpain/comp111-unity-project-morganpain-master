c17373a9dfba0377f55230c731921fbdee2806f5
morganpain <53803484+morganpain@users.noreply.github.com>
2019-10-15 13:10:23 +1100

    Initial commit

---
+++ COMP111 Unity Assignment/Assets/_Game -- YOUR ASSETS GO HERE/Scripts/ClickToAnim.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToAnim : MonoBehaviour {
+
+	public Animator animator;
+	public string lookAtTriggerName = "LookAt";
+	public string lookAwayTriggerName = "LookAway";
+	public string lookingAtBoolName = "LookingAt";
+
+	public string mouseDownTriggerName = "MouseDown";
+	public string mouseUpTriggerName = "MouseUp";
+	public string mouseDragBoolName = "MouseDrag";
+	private bool isMouseOver = false;
+	private bool isMouseDrag = false;
+	private new Camera camera;
+
+	void Update () {
+
+		 RaycastHit hitInfo;
+
+		 Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+ 		if (Physics.Raycast(ray, out hitInfo) && hitInfo.collider.gameObject == gameObject) {
+ 			if (!isMouseOver) {
+				animator.SetTrigger(lookAtTriggerName);
+	 			isMouseOver = true;
+ 			}
+
+ 			if (Input.GetMouseButtonDown(0)) {
+				animator.SetTrigger(mouseDownTriggerName);
+				isMouseDrag = true;
+ 			}
+
+
+ 		}
+ 		else {
+ 			if (isMouseOver) {
+				animator.SetTrigger(lookAwayTriggerName);
+	 			isMouseOver = false;
+ 			}
+ 		}
+
+		if (Input.GetMouseButtonUp(0) && isMouseDrag) {
+			animator.SetTrigger(mouseUpTriggerName);
+			isMouseDrag = false;
+ 		}
+
+		animator.SetBool(lookingAtBoolName, isMouseOver);
+		animator.SetBool(mouseDragBoolName, isMouseDrag);
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# README #
+
+Framework for COMP111 Unity assignment. 
+
+Includes the following assets:
+* Camera asset bundle
+* Environment asset bundle
+* Effects asset bundle
+* Terrain assets
+* ProBuilder basic
+* ClickToAnim and TriggerToAnim scripts
